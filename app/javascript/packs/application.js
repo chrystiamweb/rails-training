da303728198a90a8aeba4945ab8949d61ac96de1
Chrystiam Andrade <chrystiam.andrade@gmail.com>
2021-04-13 20:37:52 -0300

    add materialize css

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,20 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import '../stylesheets/application'
+import 'materialize-css/dist/js/materialize'
+
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+document.addEventListener("turbolinks:load", function() {
+    (function($){
+        $(function(){
+      
+          $('.sidenav').sidenav();
+      
+        }); // end of document ready
+      })(jQuery); // end of jQuery name space      
+});
